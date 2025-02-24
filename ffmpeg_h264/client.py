7c3271715663cbffd510c59d9990aad6d215b018
d0v0h <00_kdh@naver.com>
2025-02-24 21:42:01 +0900

    ffmpeg 버전

--- ffmpeg_h264/client.py
+++ ffmpeg_h264/client.py
@@ -5,22 +5,37 @@ def send_video():
     SERVER_IP = "172.30.1.16"   # 예시: 서버 컴퓨터의 내부망 IP
     SERVER_PORT = 5002
 
-    width = 1640
-    height = 1232
-    fps = 25
+    width = 640
+    height = 480
+    fps = 15
+    bitrate = 0.25 * 1000 * 1000
     
-    # raspivid(또는 libcamera-vid) 명령어를 실행하여 H.264로 인코딩 후 stdout으로 내보냄
+    # # raspivid(또는 libcamera-vid) 명령어를 실행하여 H.264로 인코딩 후 stdout으로 내보냄
+    # raspivid_cmd = [
+    #     "raspivid",
+    #     "-t", "0",               # 무기한 실행
+    #     "-o", "-",               # 표준 출력(stdout)으로 H.264 바이트스트림
+    #     "-w", f"{width}",             # 해상도 예시
+    #     "-h", f"{height}",
+    #     "-fps", f"{fps}",            # 프레임레이트
+    #     "-pf", "high"            # 프로파일 설정 (고화질)
+    #     "-g 10"
+    #     # libcamera-vid 사용 시:
+    #     # "libcamera-vid", "--inline", "-t", "0", "--width", "640", "--height", "480",
+    #     # "--framerate", "30", "--codec", "h264", "-o", "-"
+    # ]
+
     raspivid_cmd = [
-        "raspivid",
-        "-t", "0",               # 무기한 실행
-        "-o", "-",               # 표준 출력(stdout)으로 H.264 바이트스트림
-        "-w", f"{width}",             # 해상도 예시
-        "-h", f"{height}",
-        "-fps", f"{fps}",            # 프레임레이트
-        "-pf", "high"            # 프로파일 설정 (고화질)
-        # libcamera-vid 사용 시:
-        # "libcamera-vid", "--inline", "-t", "0", "--width", "640", "--height", "480",
-        # "--framerate", "30", "--codec", "h264", "-o", "-"
+        "libcamera-vid",
+        "--inline",           # SPS/PPS를 스트림 내부에 삽입
+        "-t", "0",
+        "--width", f"{width}",
+        "--height", f"{height}",
+        "--framerate", f"{fps}",
+        "--intra", "5",
+        "--bitrate", f"{bitrate}"
+        "--codec", "h264",
+        "-o", "-",
     ]
     
     # FFmpeg로 piped input(표준입력: pipe:0)을 받아
@@ -35,17 +50,6 @@ def send_video():
         "-f", "mpegts",          # TS 컨테이너로 전송
         f"udp://{SERVER_IP}:{SERVER_PORT}"
     ]
-
-    # raspivid_cmd = [
-    #     "libcamera-vid",
-    #     "--inline",           # SPS/PPS를 스트림 내부에 삽입
-    #     "-t", "0",
-    #     "--width", "640",
-    #     "--height", "480",
-    #     "--framerate", "30",
-    #     "--codec", "h264",
-    #     "-o", "-"
-    # ]
     
     # Popen으로 두 프로세스를 파이프로 연결
     raspivid_proc = subprocess.Popen(raspivid_cmd, stdout=subprocess.PIPE)
